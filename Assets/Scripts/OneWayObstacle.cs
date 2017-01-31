e2703ac962b6fb9810a35b9b9a555121b8451870
Hugo Verger <hverger@wanadoo.fr>
2017-01-31 10:45:58 +0100

    OneWay obstacles

---
+++ Assets/Scripts/OneWayObstacle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayObstacle : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (collision.gameObject.transform.position.y > transform.position.y)
+            {
+                gameObject.GetComponent<Collider2D>().isTrigger = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            gameObject.GetComponent<Collider2D>().isTrigger = false;
+        }
+    }
+}
