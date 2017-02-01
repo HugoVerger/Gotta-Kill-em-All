7e20c49ba7de858173d1b0e1e9d73e056962ce48
Rémi BERTRAND <rbertrand21@gmail.com>
2017-02-01 12:44:54 +0100

    Added gun sound, text bubbles and text police

---
+++ Assets/Scripts/DisplayText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayText : MonoBehaviour
+{
+    public GameObject textBubble;
+    public Text text;
+
+    public void displayText(string newText)
+    {
+        textBubble.SetActive(true);
+        text.text = newText;
+    }
+}
