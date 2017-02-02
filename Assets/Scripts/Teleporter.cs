83e8949772559e3617fe528941d070bf95b13c70
Hugo Verger <hverger@wanadoo.fr>
2017-02-02 02:21:50 +0100

    Fade transitions for each teleporter and load level

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+    public Transform destination;
+    public Orientation orientation = Orientation.MoveUp;
+    public float delay = 1;
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.tag == "Player" && collider.gameObject.GetComponent<PlayerController>().orientation == orientation) {
+            GameObject.Find("BlackScreen").GetComponent<Animator>().Play("Fade");
+            Invoke("TeleportPlayer", delay);
+        }
+    }
+
+    void TeleportPlayer() {
+        GameObject.Find("Player").transform.position = destination.position;
+    }
+}
